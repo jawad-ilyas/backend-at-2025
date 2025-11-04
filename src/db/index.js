c56695d3fe890c3ef213dc42cf47267dca15728a
jawad mughal <mughaljawad12@gmail.com>
2025-11-04 14:21:35 -0800

    setup the db

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+
+
+const ConnectDb = async () => {
+
+    try {
+        const db = await mongoose.connect(process.env.DB_URI)
+        console.log('====================================');
+        // console.log("index.js :: ConnectDb :: db", db);
+        console.log("index.js :: ConnectDb :: db", db?.connection);
+        console.log('====================================');
+    } catch (error) {
+        console.log('====================================');
+        console.log("db/index.js :: connectDb :error :: ", error);
+        console.log('====================================');
+        throw error
+    }
+}
+
+export default ConnectDb
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import app from "./app.js";
+import ConnectDb from "./db/index.js";
+
+
+ConnectDb()
+    .then(() => {
+        console.log(process.env.port)
+        app.listen(process.env.port, () => {
+            console.log("app is listen on the ", process.env.port);
+        })
+
+    })
+    .catch((error) => {
+        console.log('====================================');
+        console.log("index.js :: connectDb index.js :error :: ", error);
+        console.log('====================================');
+        throw error
+    })
